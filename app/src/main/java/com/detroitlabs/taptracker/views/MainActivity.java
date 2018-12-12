5f92abbe167c1a02fcce0a522270f77e6d19893e
Jason Petterson <jp@brokenshotgun.com>
2018-12-12 15:40:15 -0500

    replaced fab with menu action for adding new task

--- app/src/main/java/com/detroitlabs/taptracker/views/MainActivity.java
+++ app/src/main/java/com/detroitlabs/taptracker/views/MainActivity.java
@@ -50,18 +50,20 @@ public class MainActivity extends AppCompatActivity implements MainPresenter.Vie
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        //FloatingActionButton fab = findViewById(R.id.fab);
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
         mAdapter = new TaskListAdapter(this);
         mTaskViewModel = ViewModelProviders.of(this).get(TaskViewModel.class);
 
+        /*
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 presenter.onNewTaskButtonClicked();
             }
         });
+        */
 
         mAdapter.setOnItemClickListener(new TaskListAdapter.OnItemClickListener() {
             @Override
@@ -154,12 +156,13 @@ public class MainActivity extends AppCompatActivity implements MainPresenter.Vie
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        //int id = item.getItemId();
+        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        //if (id == R.id.action_settings) {
-        //    return true;
-        //}
+        if (id == R.id.action_add_task) {
+            presenter.onNewTaskButtonClicked();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
